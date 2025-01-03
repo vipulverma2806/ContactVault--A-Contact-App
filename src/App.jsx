a1b7b8e201f9d994e8b73f547d3fa4d1a001b132
Vipul Verma <56539924+vipulverma2806@users.noreply.github.com>
2025-01-03 23:47:56 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
-
-
+import OverviewPage from "./Pages/OverviewPage";
+import ProductsPage from "./Pages/ProductsPage";
+import Sidebar from "./Components/Sidebar";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  
-
   return (
-    <>
-     <h1 className="text-red-500">tailwindnp</h1>
-    </>
-  )
+    <div className='flex h-screen ☐ bg-gray-900 text-gray-100 overflow-hidden'>
+      
+    {/* BG */}
+    <div className='fixed inset-0 z-0'>
+    <div className='absolute inset-0 bg-gradient-to-br from-gray-900  via-gray-800  to-gray-900 opacity-80' />
+    <div className='absolute inset-0 backdrop-blur-sm' />
+    </div>
+    <Sidebar/>
+      <Routes>
+        <Route path="/" element={<OverviewPage/>}></Route>
+        <Route path="/products" element={<ProductsPage/>}></Route>
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
